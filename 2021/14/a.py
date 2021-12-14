efec30dcf92e9fec01aa58f1e54019cbcf411f36
benjamin saintes <benjamin.saintes@sporteasy.net>
2021-12-14 13:56:18 +0100

    Day 14

--- 2021/14/a.py
+++ 2021/14/a.py
@@ -1,3 +1,25 @@
+from collections import Counter
+import functools
+
 with open("input") as fd:
-    for line in fd.readlines():
-        print(line)
+    poly, _, *rules = fd.readlines()
+
+poly = poly.strip()
+rules = dict(r.strip().split(" -> ") for r in rules)
+
+
+@functools.cache
+def count(p, n):
+    if n == 0 or p not in rules:
+        return Counter(p[:1])
+    else:
+        return count(p[0] + rules[p], n - 1) + count(rules[p] + p[1], n - 1)
+
+
+counter = Counter()
+for i in range(len(poly) - 1):
+    counter.update(count(poly[i:i+2], 40))
+counter += Counter(poly[-1:])
+
+l, *_, m = sorted(counter.values())
+print(m - l)
