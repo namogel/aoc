618673a0bea4822bffed09b6d5d09f276354132e
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-03 10:18:55 +0100

    Day 3

--- 2022/03/a.py
+++ 2022/03/a.py
@@ -1,3 +1,28 @@
+from string import ascii_letters
+
+priority = 0
+
+# part 1
 with open("input") as fd:
     for line in fd.readlines():
-        print(line)
+        line = line.rstrip("\n")
+        assert not len(line) % 2
+        mid = len(line) // 2
+        common = list(set(line[:mid]) & set(line[mid:]))
+        assert len(common) == 1
+        priority += ascii_letters.index(common[0]) + 1
+
+# part 2
+group = []
+with open("input") as fd:
+    for line in fd.readlines():
+        line = line.rstrip("\n")
+        group.append(set(line))
+        if len(group) < 3:
+            continue
+        common = list(set.intersection(*group))
+        assert len(common) == 1
+        priority += ascii_letters.index(common[0]) + 1
+        group = []
+
+print(priority)
