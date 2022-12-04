7cd120bb9a9dfaafc4dcc21d1e651501c2c4aee9
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-04 12:11:25 +0100

    Day 4

--- 2022/04/a.py
+++ 2022/04/a.py
@@ -1,3 +1,12 @@
+part1 = part2 = 0
+
 with open("input") as fd:
     for line in fd.readlines():
-        print(line)
+        p1, p2 = (tuple(map(int, p.split("-"))) for p in line.rstrip("\n").split(","))
+        if p1[0] <= p2[0] <= p2[1] <= p1[1] or p2[0] <= p1[0] <= p1[1] <= p2[1]:
+            part1 += 1
+        if p1[0] <= p2[0] <= p1[1] or p2[0] <= p1[0] <= p2[1]:
+            part2 += 1
+
+print(part1)
+print(part2)
