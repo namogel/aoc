0144775c795262f77a7ba158ac7b8e6c679be937
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-05 09:24:53 +0100

    Day 5

--- 2022/05/a.py
+++ 2022/05/a.py
@@ -1,3 +1,22 @@
+import re
+
+letter = re.compile(r"\w")
+move = re.compile(r"move (\d+) from (\d+) to (\d+)")
+stacks = [[] for _ in range(10)]
+
 with open("input") as fd:
     for line in fd.readlines():
-        print(line.rstrip("\n"))
+        line = line.rstrip("\n")
+        if not line.startswith(" 1 "):
+            for i in range(len(line) // 4 + 1):
+                if match := letter.search(line[i*4:i*4+4]):
+                    stacks[i + 1].insert(0, match.group())
+        if line.startswith("move"):
+            count, from_, to_ = map(int, move.search(line).groups())
+            # part 1
+            # stacks[to_].extend(stacks[from_][-count:][::-1])
+            # part 2
+            stacks[to_].extend(stacks[from_][-count:])
+            stacks[from_] = stacks[from_][:-count]
+
+print("".join(s[-1] for s in stacks[1:]))
