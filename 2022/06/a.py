12c53c44187df22825e18c48940ff330833b0a57
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-07 13:59:55 +0100

    Day 6

--- 2022/06/a.py
+++ 2022/06/a.py
@@ -1,3 +1,6 @@
 with open("input") as fd:
-    for line in fd.readlines():
-        print(line.rstrip("\n"))
+    line = fd.readlines()[0].rstrip("\n")
+
+# n = 4  # part 1
+n = 14  # part 2
+print(next(i + n for i in range(len(line) - n) if len(set(line[i:i+n])) == n))
