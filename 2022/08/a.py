a3790a183391f8a2b1ba1ea873c1f382ebce545c
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-10 09:56:06 +0100

    Day 8 - golf

--- 2022/08/a.py
+++ 2022/08/a.py
@@ -4,39 +4,26 @@ with open("input") as fd:
 WIDTH, HEIGHT = len(grid[0]), len(grid)
 
 # part 1
-visited = set()
-for j, line in enumerate(grid):
-    highest = "-1"
-    for i, h in enumerate(line):
-        if h > highest:
-            highest = h
-            visited.add((i, j))
-    highest = "-1"
-    for i, h in enumerate(line[::-1]):
-        if h > highest:
-            highest = h
-            visited.add((WIDTH-1-i, j))
-for i in range(WIDTH):
-    column = "".join(grid[j][i] for j in range(HEIGHT))
-    highest = "-1"
-    for j, h in enumerate(column):
-        if h > highest:
-            highest = h
-            visited.add((i, j))
-    highest = "-1"
-    for j, h in enumerate(column[::-1]):
-        if h > highest:
-            highest = h
-            visited.add((i, HEIGHT-1-j))
-print(len(visited))
+visible = sum(
+    1
+    for i in range(WIDTH)
+    for j in range(HEIGHT)
+    if (
+        all(grid[j][i_] < grid[j][i] for i_ in range(i))  # left
+        or all(grid[j][i_] < grid[j][i] for i_ in range(i+1, WIDTH))  # right
+        or all(grid[j_][i] < grid[j][i] for j_ in range(j))  # top
+        or all(grid[j_][i] < grid[j][i] for j_ in range(j+1, HEIGHT))  # bottom
+    )
+)
+print(visible)
 
 # part 2
 max_ = max(
     (
-        next((k for k, i_ in enumerate(range(i-1, -1, -1), 1) if grid[j][i_] >= grid[j][i]), i) *
-        next((k for k, i_ in enumerate(range(i+1, WIDTH), 1) if grid[j][i_] >= grid[j][i]), WIDTH-1-i) *
-        next((k for k, j_ in enumerate(range(j-1, -1, -1), 1) if grid[j_][i] >= grid[j][i]), j) *
-        next((k for k, j_ in enumerate(range(j+1, HEIGHT), 1) if grid[j_][i] >= grid[j][i]), HEIGHT-1-j)
+        next((k for k, i_ in enumerate(range(i-1, -1, -1), 1) if grid[j][i_] >= grid[j][i]), i) *  # left
+        next((k for k, i_ in enumerate(range(i+1, WIDTH), 1) if grid[j][i_] >= grid[j][i]), WIDTH-1-i) *  # right
+        next((k for k, j_ in enumerate(range(j-1, -1, -1), 1) if grid[j_][i] >= grid[j][i]), j) *  # top
+        next((k for k, j_ in enumerate(range(j+1, HEIGHT), 1) if grid[j_][i] >= grid[j][i]), HEIGHT-1-j)  # bottom
     )
     for i in range(1, WIDTH-1)
     for j in range(1, HEIGHT-1)
