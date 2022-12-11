7431a924d919c204c325f091c4a9595413fc7417
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-11 13:46:46 +0100

    Day 9

--- 2022/09/a.py
+++ 2022/09/a.py
@@ -1,3 +1,21 @@
+xh, yh = 0, 0
+visited = set()
+# LENGTH = 1  # part 1
+LENGTH = 9  # part 2
+
+knots = [[0, 0] for _ in range(LENGTH)]
 with open("input") as fd:
     for line in fd.readlines():
-        print(line.rstrip("\n"))
+        direction, count = line.rstrip("\n").split()
+        for _ in range(int(count)):
+            xh += {"L": -1, "R": 1}.get(direction, 0)
+            yh += {"U": -1, "D": 1}.get(direction, 0)
+            for i in range(LENGTH):
+                x2, y2 = knots[i]
+                x1, y1 = (xh, yh) if i == 0 else knots[i-1]
+                if (y2 - y1)**2 > 1 or (x2 - x1)**2 > 1:
+                    knots[i][0] += (1 if x1 > x2 else -1) * (x1 != x2)
+                    knots[i][1] += 1 if y1 > y2 else -1 * (y2 != y1)
+            visited.add(tuple(knots[-1]))
+
+print(len(visited))
