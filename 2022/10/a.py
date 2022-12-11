36ebaf8e991e8ba8aad0896e135c51e2b230a151
benjamin saintes <benjamin.saintes@sporteasy.net>
2022-12-11 14:49:58 +0100

    Day 10

--- 2022/10/a.py
+++ 2022/10/a.py
@@ -1,3 +1,21 @@
+x, ops, s = 1, [], 0
 with open("input") as fd:
     for line in fd.readlines():
-        print(line.rstrip("\n"))
+        line = line.rstrip("\n")
+        ops.append(0)
+        if line != "noop":
+            ops.append(int(line.split()[1]))
+
+# part1
+for i, v in enumerate(ops, 1):
+    if i == 20 or not ((i + 20) % 40):
+        s += x * i
+    x += v
+print(s)
+
+# part2
+for i, v in enumerate(ops):
+    print("#" if i % 40 in (x - 1, x, x + 1) else ".", end="")
+    x += v
+    if not (i+1) % 40:
+        print()
